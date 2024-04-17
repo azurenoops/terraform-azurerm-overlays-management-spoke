79cf5bfd451bd246a5f20cf24d12e8fa8ec31995
John Spinella <jrspinella@users.noreply.github.com>
2024-04-17 13:49:50 -0400

    add storage blob contrib

--- resources.management.spoke.role.tf
+++ resources.management.spoke.role.tf
@@ -7,3 +7,9 @@ resource "azurerm_role_assignment" "peering" {
   role_definition_name = "Network Contributor"
   principal_id         = data.azurerm_client_config.current.object_id
 }
+
+resource "azurerm_role_assignment" "storage" {
+  scope                = module.spoke_st.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
