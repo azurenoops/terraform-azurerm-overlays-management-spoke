791a2c8c447a67e68a419854dcce762c28234a19
John Spinella <jrspinella@users.noreply.github.com>
2024-04-19 09:42:43 -0400

    Updates to examples

--- examples/Commerical/basic_mgt_spoke/data.tf
+++ examples/Commerical/basic_mgt_spoke/data.tf
@@ -1,24 +1,20 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_virtual_network" "hub-vnet" {
-  provider = azurerm.hub
   name                = "an1-eus-hub-dev-vnet"
   resource_group_name = "an1-eus-hub-dev-rg"
 }
 
 data "azurerm_firewall" "hub-fw" {
-  provider = azurerm.hub
   name                = "an1-eus-hub-dev-fw"
   resource_group_name = "an1-eus-hub-dev-rg"
 }
 
 data "azurerm_log_analytics_workspace" "hub-logws" {
-  provider = azurerm.hub
   name                = "laws-eastus-an1"
   resource_group_name = "laws-rg-eastus-an1"
 }
 
 data "azurerm_resource_group" "dns" {
-  provider = azurerm.hub
   name       = "an1-eus-dns-dev-rg"
 }
--- examples/Government/basic_mgt_spoke/data.tf
+++ examples/Government/basic_mgt_spoke/data.tf
@@ -1,24 +1,20 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_virtual_network" "hub-vnet" {
-  provider            = azurerm.hub
   name                = "an1-usgva-hub-dev-vnet"
   resource_group_name = "an1-usgva-hub-dev-rg"
 }
 
 data "azurerm_firewall" "hub-fw" {
-  provider            = azurerm.hub
   name                = "an1-usgva-hub-dev-fw"
   resource_group_name = "an1-usgva-hub-dev-rg"
 }
 
 data "azurerm_log_analytics_workspace" "hub-logws" {
-  provider            = azurerm.ops
   name                = "laws-usgva-an1"
   resource_group_name = "laws-rg-usgva-an1"
 }
 
 data "azurerm_resource_group" "dns" {
-  provider = azurerm.hub
   name     = "an1-eus-dns-dev-rg"
 }
